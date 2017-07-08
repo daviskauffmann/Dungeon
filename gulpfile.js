eafe2d672a76e1cc7ffbb9f73e7550e0413f99fa
daviskauffmann <daviskauffmann@gmail.com>
2017-07-08 02:26:01 -0400

    Start adding tslint

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const inject = require('gulp-inject');
 const sass = require('gulp-sass');
 const sequence = require('gulp-sequence');
 const sourcemaps = require('gulp-sourcemaps');
+// const tslint = require('gulp-tslint');
 const typescript = require('gulp-typescript').createProject('tsconfig.json');
 
 gulp.task('watch', ['build'], () => {
@@ -13,13 +14,19 @@ gulp.task('watch', ['build'], () => {
 })
 
 gulp.task('build', callback => {
-    sequence('clean', 'typescript', 'sass', 'inject')(callback);
+    sequence('clean', 'tslint', 'typescript', 'sass', 'inject')(callback);
 });
 
 gulp.task('clean', () => {
     return gulp.src('./www')
         .pipe(clean());
 });
+
+// gulp.task('tslint', () => {
+//     return gulp.src('./src/ts/**/*.ts')
+//         .pipe(tslint())
+//         .pipe(tslint.report());
+// });
 
 gulp.task('typescript', () => {
     return gulp.src('./src/ts/**/*.ts')
